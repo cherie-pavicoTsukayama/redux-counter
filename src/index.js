928915fed9a8eeae9f3d9d5aaeefaf0e8d9ffc54
Cherie <cherietsukayama@gmail.com>
2020-07-13 23:06:45 +0000

    created actions and reducers folder

--- src/index.js
+++ src/index.js
@@ -3,41 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
 
-//STORE (a globalized state)
-
-//ACTION (Increment)
-const increment = () => {
-  return {
-    type:'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-//Reducer (descrie how the current store changes into the new store)
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-        return state + 1;
-    case "DECREMENT":
-        return state - 1;
-  }
-}
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log('Store:', store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
