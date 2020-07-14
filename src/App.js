a817c33509f6f1eec358cb4220ac461f6622a411
Cherie <cherietsukayama@gmail.com>
2020-07-14 02:43:21 +0000

    added the counter reducer, isLogged reduccer and the added the const increment and decrement to the index.js file in actions.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
+  function displayLogMessage() {
+    return(
+      isLogged ? <h3>Here's is your info</h3>
+      : <h3>You must be logged in.</h3>
+    )
+  }
+
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>Counter {counter}</h1>
+      <button>
+        +
+      </button>
+      <button>
+        -
+      </button>
+
+      {displayLogMessage()}
+    </div>
 
 
   );
